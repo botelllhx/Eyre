aadb91f528a60f77fd5939e093cd71decb9e466e
botelllhx <maat9779@gmail.com>
2023-07-26 20:02:42 -0300

    Eyre_v1

--- pacote/script.js
+++ pacote/script.js
@@ -17,3 +17,132 @@ function formatarMoeda(campo) {
 
     campo.value = "R$ " + parseFloat(valor).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
 }
+
+
+function toggleCheck(checkbox) {
+    const side = checkbox.getAttribute('data-side');
+    const checkboxes = document.querySelectorAll(`[data-side="${side}"]`);
+    const isCheckboxChecked = checkbox.classList.contains("checked");
+
+    checkboxes.forEach((box) => {
+        box.classList.remove("checked");
+    });
+
+    if (!isCheckboxChecked) {
+        checkbox.classList.add("checked");
+    }
+}
+
+
+function toggleSelection(button) {
+    const buttons = document.querySelectorAll(".botaoSelecionar");
+
+    buttons.forEach((btn) => {
+        if (btn === button) {
+            if (btn.classList.contains("selected")) {
+                btn.classList.remove("selected");
+                btn.innerText = "Selecionar";
+            } else {
+                btn.classList.add("selected");
+                btn.innerText = "Selecionado";
+            }
+        } else {
+            btn.classList.remove("selected");
+            btn.innerText = "Selecionar";
+        }
+    });
+}
+
+
+let botaoSelecionarAtivo = false;
+
+function toggleSelectionCategoria(categoria) {
+    if (!botaoSelecionarAtivo) {
+        const buttons = document.getElementsByClassName("botaoCategoriaLugares");
+        for (const btn of buttons) {
+            btn.classList.remove("botaoAtivo");
+        }
+        botaoSelecionarAtivo = true;
+
+        const turisticoContent = document.getElementById("turisticoContent");
+        turisticoContent.style.display = "flex";
+    }
+
+    const contents = document.getElementsByClassName("categoriaContent");
+    for (const content of contents) {
+        content.style.display = "none";
+    }
+
+    const contentToShow = document.getElementById(categoria + "Content");
+    if (contentToShow) {
+        contentToShow.style.display = "flex";
+    }
+
+    const buttons = document.getElementsByClassName("botaoCategoriaLugares");
+    for (const btn of buttons) {
+        btn.classList.remove("botaoAtivo");
+    }
+    const button = document.getElementById(categoria);
+    button.classList.add("botaoAtivo");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const turisticoButton = document.getElementById("turistico");
+    turisticoButton.classList.add("botaoAtivo");
+    toggleSelectionCategoria("turistico");
+});
+
+
+let modalIndex = 0;
+
+function abrirModal() {
+    document.getElementById('meuModal').style.display = 'block';
+    mostrarImagem(modalIndex);
+}
+
+function fecharModal() {
+    document.getElementById('meuModal').style.display = 'none';
+}
+
+function mostrarImagem(index) {
+    const imagens = document.getElementsByClassName('modal-conteudo');
+    if (index >= 0 && index < imagens.length) {
+        modalIndex = index;
+        for (let i = 0; i < imagens.length; i++) {
+            imagens[i].style.display = 'none';
+        }
+        imagens[modalIndex].style.display = 'block';
+    }
+}
+
+function avancarImagem() {
+    mostrarImagem(modalIndex + 1);
+}
+
+function retrocederImagem() {
+    mostrarImagem(modalIndex - 1);
+}
+
+document.onkeydown = function (event) {
+    event = event || window.event;
+    if (event.keyCode == 37) {
+        retrocederImagem();
+    } else if (event.keyCode == 39) {
+        avancarImagem();
+    }
+};
+
+
+function abrirModalMapa() {
+    var modal = document.getElementById('mapModal');
+    if (modal) {
+        modal.style.display = 'flex';
+    }
+}
+
+function fecharModalMapa() {
+    var modal = document.getElementById('mapModal');
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
