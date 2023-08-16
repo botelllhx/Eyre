62092e25db5a59b8ec9e168820f1e06f79bc8297
botelllhx <maat9779@gmail.com>
2023-08-16 19:06:09 -0300

    Eyre_v2

---
+++ banco/testeDiegoMap.js
@@ -0,0 +1,6 @@
+// var map = new Map();
+// map.set("Opa", {id:1,nome:"bom"});
+// map.set("AiAi", [{id:1,nome:"bom"},{id:2,nome:"qwert"}]);
+// console.log(map);
+// console.log(map.entries());
+// console.log(map.get("AiAi")[1].id);
